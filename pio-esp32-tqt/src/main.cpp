6a799dc1bb7614d333db0a5ef0074bf75157ebdf
Cyrill von Wattenwyl <cyrill@peerdom.org>
2022-10-11 10:53:27 +0200

    add lilygo t-qt

---
+++ pio-esp32-tqt/src/main.cpp
@@ -0,0 +1,27 @@
+#include "Arduino.h"
+#include "config.h"
+
+// uncomment for display
+// #include <TFT_eSPI.h> 
+// #include <SPI.h>
+// #include <Wire.h>
+
+// TFT_eSPI tft = TFT_eSPI(TFT_WIDTH, TFT_HEIGHT); 
+// void init_display(){
+//   tft.init();
+//   pinMode(TFT_BL, OUTPUT);
+//   digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); 
+//   tft.setSwapBytes(true);
+//   tft.setRotation(TFT_ROTATION);
+//   tft.fillScreen(TFT_BLACK);
+// }
+
+void setup() {
+
+}
+
+
+void loop() {
+  
+}
+ 
